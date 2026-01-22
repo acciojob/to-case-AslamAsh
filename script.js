26dd473b5af7eb2ec200fe0e4f72cafbdf35f1a3
AslamAsh <93033181+AslamAsh@users.noreply.github.com>
2026-01-22 22:35:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function toCase(text) {
   // write your code here
+	if (!str || str.length === 0) {
+    return "-";
+  }
+
+  return str.toLowerCase() + "-" + str.toUpperCase();
+}
 }
 
 // DO not change the code below
